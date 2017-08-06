3e23d2eb95c62796de2c8ab9b9a9ba497ff57e68
Apex-Code <upstaterounder@gmail.com>
2017-08-06 14:41:51 -0500

    add private methods and functionality for goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,25 +1,48 @@
 class GoalsController < ApplicationController
 
   def index
+    @goals = Goal.all.order("created_at DESC")
   end
 
   def show
   end
-  
+
   def new
+    @goal = Goal.new
   end
 
   def create
+    @goal = Goal.new(goal_params)
+    if @goal.save
+      redirect_to @goal
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @goal.update(goal_params)
+      redirect_to @goal
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @goal.destroy
   end
 
+private
+
+  def goal_params
+    params.require(:goal).permit(:title, :target, :balance, :target_date)
+  end
+
+  def find_goal
+    @goal = Goal.find(params[:id])
+  end
 
 end
