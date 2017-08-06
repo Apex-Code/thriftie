31ebd41b108c50348c67ca317c6ab5f1e103ea1e
Apex-Code <upstaterounder@gmail.com>
2017-08-06 14:49:28 -0500

    adjust goals migration

--- db/migrate/20170806194807_create_goals.rb
+++ db/migrate/20170806194807_create_goals.rb
@@ -1,11 +1,10 @@
 class CreateGoals < ActiveRecord::Migration[5.1]
   def change
     create_table :goals do |t|
-      t.string :title
-      t.integer :target
-      t.integer :balance
+      t.string   :title
+      t.integer  :target
+      t.integer  :balance
       t.datetime :target_date
-
       t.timestamps
     end
   end
