943174ae8b17d290a0752c21d2dc6d777bf9364e
Apex-Code <upstaterounder@gmail.com>
2017-08-06 14:32:43 -0500

    add goals controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'goals/index'
+
+  get 'goals/show'
+
+  get 'goals/edit'
+
+  get 'goals/_form'
+
+  get 'goals/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
