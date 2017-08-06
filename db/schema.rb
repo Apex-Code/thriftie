49b2b93c25ff9ea30dd4281bd42c355a9861072c
Apex-Code <upstaterounder@gmail.com>
2017-08-06 15:43:11 -0500

    change table for goals.  correct syntax, migrate db :

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170806202420) do
+ActiveRecord::Schema.define(version: 20170806204057) do
 
   create_table "goals", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,8 @@ ActiveRecord::Schema.define(version: 20170806202420) do
     t.datetime "target_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
