cbf0d54cba2ffadb4c8dfa24919626c7e576e28b
Apex-Code <upstaterounder@gmail.com>
2017-08-06 15:00:24 -0500

    added jquery gem and updated app.js

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'goals/index'
-
-  get 'goals/show'
-
-  get 'goals/edit'
-
-  get 'goals/_form'
-
-  get 'goals/new'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "goals#index"
+  resource :goals
 end
