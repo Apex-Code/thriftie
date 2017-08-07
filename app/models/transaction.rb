41785f325bd7aeb3c4349825e90f782ccf1293d6
Apex-Code <upstaterounder@gmail.com>
2017-08-07 00:22:58 -0500

    added associations for goals, and transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,5 @@
 class Transaction < ApplicationRecord
   has_many :withdrawals, inverse_of: :transaction
   has_many :deposits, inverse_of: :transaction
+  belongs_to :goal, inverse_of: :transactions 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :goals, inverse_of: :user
+  has_many :transactions, through: :goals 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
