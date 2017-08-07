983e398e6a74083b67beb996fd5d374716b20e1e
Apex-Code <upstaterounder@gmail.com>
2017-08-06 22:57:29 -0500

    add has many transactions to goal model :

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,7 @@
 class Goal < ApplicationRecord
   belongs_to :user, inverse_of: :goals
   has_many :withdrawals, inverse_of: :goal
+  has_many :transactions, inverse_of: :goal
 
   def total_withdrawals
     @goal.withdrawals.sum
