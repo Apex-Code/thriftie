fcfda3683666cadae4e0a693c8cdf131ebffd06a
Apex-Code <upstaterounder@gmail.com>
2017-08-07 00:42:49 -0500

    start working on transaction controller

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,24 @@
 class TransactionsController < ApplicationController
+
+  before_action :find_transaction, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
+  def new
+    @transaction = Transaction.new
+  end
+
+  def create
+    @transaction = Transaction.new(transaction_params)
+  end
+
+
+private
+
+  def transaction_params
+    params.require(:transaction).permit(:withrawal, :deposit)
+  end
+
+  def find_transaction
+    @transaction = Transaction.find(params[:id])
+  end
 end
