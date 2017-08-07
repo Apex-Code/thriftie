8c753a168df5844ec7ea15826d3bfa4cefbde9c4
Apex-Code <upstaterounder@gmail.com>
2017-08-07 15:03:58 -0500

    change file names to reflect tranzaction

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
-  resources :goals
+  resources :goals do
+    resources :tranzactions 
+  end
 
   authenticated :user do
     root "goals#index", as: "authenticated_root"
